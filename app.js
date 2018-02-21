11119bd7fe5917f26dbd4f533e002f726f757ef7
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2018-02-21 22:26:16 +0200

    add(todoController): init controller

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let express = require ('express');
 let app = express('view engine', 'ejs');
+let todoController = require('./controllers/todoController');
 
 app.set('/assets', express);
 app.use(express.static('./public'));
+
+todoController(app);
 
 app.listen(3000);
 console.log('You are listening port 3000');
